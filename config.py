1e5d525cc9d081045b4852433c437f6f59eaf183
Rampartblade <rickyadiputra98@gmail.com>
2021-11-15 13:57:50 +0700

    changes requirements

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ app = connex_app.app
 
 # Configure the SQLAlchemy part of the app instance
 app.config['SQLALCHEMY_ECHO'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://' + os.path.join(basedir, 'people.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://hypqxhozvemyyx:e2b0749b2a451e118c50e8ac88d8032541bdd2e307b556afbfca16606f1c360e@ec2-34-198-189-252.compute-1.amazonaws.com:5432/d1tuokk8562ndg'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Create the SQLAlchemy db instance
